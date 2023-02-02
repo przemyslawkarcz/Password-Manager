f6bb8b40117dda2994b5462c1d0f2549a0ba9648
Karcz Przemysław <pkarc@PGI.LOCAL>
2023-02-02 14:55:43 +0100

    The Characters module has been created. In it, several classes were created. All methods in these classes are protected. Created a facade class in this module with public methods.

---
+++ src/main/java/Password_Generator/Characters/CharactersFacade.java
@@ -0,0 +1,12 @@
+package Password_Generator.Characters;
+
+public class CharactersFacade {
+
+    public void getIndividualCharacters(){
+
+        CharactersStatistics charactersStatistics = new CharactersStatistics();
+        charactersStatistics.countsIndividualCharactersOfPassword();
+
+    }
+
+}
---
+++ src/main/java/Password_Generator/Characters/CharactersStatistics.java
@@ -0,0 +1,44 @@
+package Password_Generator.Characters;
+
+import Password_Generator.Verification.Strength;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharactersStatistics {
+
+    protected void countsIndividualCharactersOfPassword() {
+
+        Map<Character, Integer> characterIntegerMap = new HashMap<>();
+
+        String lineOfText = String.valueOf(Strength.password);
+
+        char[] charArray = lineOfText.toCharArray();
+
+        for (char c : charArray) {
+
+            if (!characterIntegerMap.containsKey(c)) {
+
+                characterIntegerMap.put(c, 1);
+
+            } else {
+
+                Integer counter = characterIntegerMap.get(c);
+                counter++;
+                characterIntegerMap.put(c, counter);
+            }
+
+        }
+
+        System.out.print("Characters in password individually: ");
+        for (Map.Entry<Character, Integer> map:characterIntegerMap.entrySet()) {
+
+            Character key = map.getKey();
+            Integer value = map.getValue();
+
+            System.out.print(key + ":" + value + " | ");
+
+        }
+
+    }
+
+}

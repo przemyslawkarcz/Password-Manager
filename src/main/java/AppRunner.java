59206418d76823dc5d9c82ebe0ab2ec662cbd3cc
Karcz Przemysław <pkarc@PGI.LOCAL>
2023-02-02 14:59:51 +0100

    created one getGeneratedPassword method to generate passwords and placed it in the AppRunner class

---
+++ src/main/java/AppRunner.java
@@ -0,0 +1,20 @@
+
+import Password_Generator.Creator.CreatorFacade;
+
+public class AppRunner {
+
+    public static void main(String[] args) {
+
+        CreatorFacade creatorFacade = new CreatorFacade();
+
+
+
+
+        creatorFacade.getGeneratedPassword();
+
+
+
+
+    }
+
+}

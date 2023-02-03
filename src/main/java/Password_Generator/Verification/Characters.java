ee719e23a9cac8149a23a37846efae6d13e459d5
Karcz Przemysław <pkarc@PGI.LOCAL>
2023-02-03 12:54:09 +0100

    Added tests in the CharactersTest test class. The added tests concern the checksNumberOfCharacters method from the Characters class located in the Password_Generator/Verification module.

--- src/main/java/Password_Generator/Verification/Characters.java
+++ src/main/java/Password_Generator/Verification/Characters.java
@@ -4,9 +4,11 @@ import static Password_Generator.Verification.Strength.password;
 
 public class Characters {
 
-    protected void checksNumberOfCharacters(){
+    protected Integer checksNumberOfCharacters(){
 
         System.out.println("Password length: " + password.length() + " character/s.");
+
+        return password.length();
 
     }
 
--- src/test/java/Password_Generator/Verification/CharactersTest.java
+++ src/test/java/Password_Generator/Verification/CharactersTest.java
@@ -1,4 +1,38 @@
 package Password_Generator.Verification;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CharactersTest {
+
+    @Test
+    public void checksNumberOfCharactersTestLength01(){
+
+        Characters characters = new Characters();
+        Integer integer = characters.checksNumberOfCharacters();
+
+        Assertions.assertNotEquals(17, integer);
+
+    }
+
+    @Test
+    public void checksNumberOfCharactersTestLength02(){
+
+        Characters characters = new Characters();
+        Integer integer = characters.checksNumberOfCharacters();
+
+        Assertions.assertEquals(16, integer);
+
+    }
+
+    @Test
+    public void checksNumberOfCharactersTestNotNull(){
+
+        Characters characters = new Characters();
+        Integer integer = characters.checksNumberOfCharacters();
+
+        Assertions.assertNotNull(integer);
+
+    }
+
 }

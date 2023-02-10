8cfe9c9dd6243c2fa701d94047bcf3b2b286e71f
Karcz Przemysław <pkarc@PGI.LOCAL>
2023-02-10 12:22:21 +0100

    AppRunner class updated with method which verify password strength no I

--- src/main/java/AppRunner.java
+++ src/main/java/AppRunner.java
@@ -1,17 +1,16 @@
-
 import Password_Generator.Creator.CreatorFacade;
+import Password_Strength_Verifier_I.Creator_PSVI.CreatorFacade_PSVI;
 
 public class AppRunner {
 
     public static void main(String[] args) {
 
         CreatorFacade creatorFacade = new CreatorFacade();
-
-
+        CreatorFacade_PSVI creatorFacadePSVI = new CreatorFacade_PSVI();
 
 
         creatorFacade.getGeneratedPassword();
-
+        creatorFacadePSVI.getHandMadePassword();
 
 
 
